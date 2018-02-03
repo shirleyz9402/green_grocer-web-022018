d5095bb8a78d507c97676e51460369acc89012ce
Shirley Zhang <shirleyz9402@yahoo.com>
2018-02-03 23:44:58 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,63 @@
 def consolidate_cart(cart)
-  # code here
+  consolidated = {}
+  cart.each do |item|
+    item.each do |name, details|
+      if consolidated.keys.include?(name) == false
+        consolidated[name] = details.merge({:count => 1})
+      else 
+        consolidated[name][:count] += 1
+      end 
+    end 
+  end 
+  consolidated
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+ discount = {}
+  coupons.each do |items|
+    if cart.keys.include?(items[:item]) == true && cart[(items[:item])][:count] >= items[:num]
+      discount[(items[:item])] = {
+        :price => cart[(items[:item])][:price],
+        :clearance => cart[(items[:item])][:clearance],
+        :count => (cart[(items[:item])][:count] % items[:num])
+        }
+      discount["#{(items[:item])} W/COUPON"] = {
+        :price => items[:cost],
+        :clearance => cart[(items[:item])][:clearance],
+        :count => (cart[(items[:item])][:count] - discount[(items[:item])][:count])/items[:num]
+      }
+    end 
+  end 
+  cart.each do |name, details|
+      if discount.keys.include?(name) == false
+        discount[name] = {
+          :price => details[:price],
+          :clearance => details[:clearance],
+          :count => details[:count]
+        }
+      end 
+    end 
+  discount 
 end
 
 def apply_clearance(cart)
-  # code here
+   cart.each do |name, details|
+     if details[:clearance] == true
+      details[:price] = (details[:price] * 0.8).round(2)
+      
+    end 
+  end 
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  cart1 = consolidate_cart(cart)
+  cart2 = apply_coupons(cart1,coupons)
+  final = apply_clearance(cart2)
+  total = 0
+  final.each do |name,details|
+    total += details[:price] * details[:count]
+  end 
+  total > 100 ? total * 0.9 : total
+  
 end
